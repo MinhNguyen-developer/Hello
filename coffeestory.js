acbfd8cdd4a3257f696b5f91395921cf8a698d17
MinhNguyen-developer <85623347+MinhNguyen-developer@users.noreply.github.com>
2021-08-12 03:40:58 +0700

    Add files via upload

---
+++ coffeestory.js
@@ -0,0 +1,61 @@
+$(function(){
+
+    window.sr = ScrollReveal();
+  
+    if ($(window).width() < 768) {
+  
+        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    } else {
+        
+        sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+  
+  });
+
+function myFunction() {
+  var x = document.getElementById('myTopnav')
+  if (x.className === 'topnav') {
+    x.className += 'responsive'
+  } else {
+    x.className = 'topnav'
+  }
+}
+
